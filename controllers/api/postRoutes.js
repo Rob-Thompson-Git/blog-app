b2ae466fa3a37da316bbe77e1e7391224f8507b0
RobThompson <rob.thomp15@gmail.com>
2022-10-24 19:03:48 -0500

    added postRoutes

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,42 @@
+const router = require(".");
+const { Post } = require("../../models");
+
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Post.findAll({
+            include: [
+            {model: User}
+            ]
+        })
+        res.status(200).json(posts)
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const onePost = await Post.findByPk(req.params.id, {
+            include: [
+                {model: User}
+            ],
+        });
+        if (!onePost) {
+            res.status(404).json({message: 'No post with that id'});
+            return;
+        } else {
+            res.status(200).json(onePost);
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const createPost = await Post.create(req.body);
+        res.status(200).json(createPost);
+    } catch (err) {
+        re.status(500).json(err);
+    }
+});
--- server.js
+++ server.js
@@ -35,10 +35,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
-// app.get('/', (req, res) => {
-//   res.send(`<h1>Hi</h1>`)
-// })
-
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening', PORT));
 });
